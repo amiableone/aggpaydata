b3b5bd0e906e32c3b925462c6da3ccdf68905c96
Albert Safin <albertsafinf@gmail.com>
2024-05-25 18:45:16 +0500

    Create class data.BSONReader to read data from bson files. Put sample_collection.bson into aggregate_payments_test/data directory.

---
+++ env-rpl-test-attempt/aggregate_payments_test/data.py
@@ -0,0 +1,22 @@
+from bson import decode_all
+from bson.raw_bson import RawBSONDocument
+from pathlib import Path
+
+
+class BSONReader:
+    path = Path("data")
+    file_name = "sample_collection.bson"
+
+    def __init__(self, file_name=""):
+        """
+        Provide a name of bson file.
+        Default is "sample_collection.bson"
+
+        The file must be located in directory 'data'.
+        """
+        self.file = self.path / (file_name or self.file_name)
+
+    def read(self):
+        with open(self.file, "rb") as f:
+            self.docs = decode_all(f.read())
+        return self.docs

5222bc33193a96c7fd9fd3dce09c845c9645e54a
Albert Safin <albertsafinf@gmail.com>
2024-05-26 14:00:11 +0500

    Create a stage that filters out documents with dates outside the provided range.

---
+++ env-rpl-test-attempt/aggregate_payments_test/aggregation.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from pprint import pprint
+from pymongo import MongoClient
+
+client = MongoClient()
+sample_db = client.sample_db
+sample_coll = sample_db.sample_collection
+
+input_data = {
+    "dt_from": "2022-09-01T00:00:00",
+    "dt_upto": "2022-12-31T23:59:59",
+    "group_type": "month",
+}
+
+pipeline = [
+    {
+        # Filter out documents with dates outside the provided range
+        "$match": {
+            "dt": {
+                "$gte": datetime.fromisoformat(input_data["dt_from"]),
+                "$lte": datetime.fromisoformat(input_data["dt_upto"]),
+            },
+        },
+    },
+]
